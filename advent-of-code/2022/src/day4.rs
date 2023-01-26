eafa5dacc739d639eb3e01a6fe32e2469a9e06d8
Foxicution <foxicution@gmail.com>
2023-01-26 23:52:38 +0200

    fixed day4 solution

--- advent-of-code/2022/src/day4.rs
+++ advent-of-code/2022/src/day4.rs
@@ -1,6 +1,6 @@
-/// The solution filters elements in each line only keeping the numbers. 
-/// Then it splits the numbers into chunks of 4 (start and end for 2 entities). 
-/// Then it compares the start and end entities searching if one is inside the other. 
+/// The solution filters elements in each line only keeping the numbers.
+/// Then it splits the numbers into chunks of 4 (start and end for 2 entities).
+/// Then it compares the start and end entities searching if one is inside the other.
 /// If it is, it returns true, otherwise false.
 /// ```
 /// use advent_of_code_2022_rust::day4::day4;
@@ -13,16 +13,26 @@
 /// assert_eq!(day4(input), 2);
 /// ```
 pub fn day4(input: &str) -> usize {
-    input.lines().filter(|line| {
-        match line.as_bytes().iter().filter(|&n| b"0123456789".contains(n)).collect::<Vec<_>>().chunks_exact(4).next().unwrap()
-        {
-            &[a1, a2, b1, b2] => if a1 >= b1 && a2 <= b2 || a1 <= b1 && a2 >= b2 {
-                true
-            } else {
-                false
+    input
+        .lines()
+        .filter(|line| {
+            match line
+                .split(",")
+                .flat_map(|elf| elf.split("-").map(|num| num.parse::<usize>().unwrap()))
+                .collect::<Vec<_>>()
+                .chunks_exact(4)
+                .next()
+                .unwrap()
+            {
+                &[a1, a2, b1, b2] => {
+                    if a1 >= b1 && a2 <= b2 || a1 <= b1 && a2 >= b2 {
+                        true
+                    } else {
+                        false
+                    }
+                }
+                _ => false,
             }
-            _ => false,
-            
-        }
-    }).count()
+        })
+        .count()
 }
