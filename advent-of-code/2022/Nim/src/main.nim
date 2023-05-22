9eb9d76dc6f45188473b60027f258aab43b5a469
Foxicution <foxicution@gmail.com>
2023-05-22 21:17:14 +0300

    nim aoc 2022 day3b solution

--- advent-of-code/2022/Nim/src/main.nim
+++ advent-of-code/2022/Nim/src/main.nim
@@ -5,7 +5,7 @@ import Solutions/[day1, day2, day3]
 let answers = {
     1: @[day1a, day1b],
     2: @[day2a, day2b],
-    3: @[day3a]
+    3: @[day3a, day3b]
     }.toOrderedTable
 
 for k, funcs in answers.pairs:
